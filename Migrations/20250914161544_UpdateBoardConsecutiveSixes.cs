541f935086c1286f8d09220a78f7238cce188711
Jordi Yaputra <jordiyaputra@gmail.com>
2025-09-14 23:16:27 +0700

    feat: add migration for board consecutive 6 column

---
+++ Migrations/20250914161544_UpdateBoardConsecutiveSixes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LudoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBoardConsecutiveSixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastConsecutiveSixes",
+                table: "Boards",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastConsecutiveSixes",
+                table: "Boards");
+        }
+    }
+}
